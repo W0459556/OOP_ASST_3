26e64f91fe09a098381bc743496d2920ce091151
Breanna Young <w0459556@campus.nscc.ca>
2024-04-03 17:50:25 -0300

    oh god i dont even know

---
+++ src/ca/nscc/GUI/ChooseFrame.java
@@ -0,0 +1,18 @@
+package ca.nscc.GUI;
+//import ca.nscc.Classes.Rogue;
+//import ca.nscc.Classes.Wizard;
+//import ca.nscc.Classes.Enemy;
+
+import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.util.Objects;
+class ChooseFrame extends JFrame {
+    public ChooseFrame() {
+        setTitle("Choose Frame");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+}
---
+++ src/ca/nscc/GUI/splashPanel.java
@@ -0,0 +1,2 @@
+package ca.nscc.GUI;public class splashPanel {
+}
